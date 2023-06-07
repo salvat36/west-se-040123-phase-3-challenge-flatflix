9a685fd0df6124ef78e1b274f816b8cd663fed81
Ryan <salvat36@gmail.com>
2023-06-06 20:27:48 -0500

    passing all

--- code-challenge/lib/classes/viewer.py
+++ code-challenge/lib/classes/viewer.py
@@ -17,16 +17,22 @@ class Viewer:
         else:
             raise AttributeError("Must be of type string and between 6-16 characters")
     
+    #returns a list of all reviews for a specific viewer(self)
     def reviews(self):
         return [review for review in Review.all if review.viewer == self]
     
+    #returns a list of all reviewed movies for a specific viewer(self)
     def reviewed_movies(self):
         return [review.movie for review in self.reviews() if review.viewer == self]
 
 #if there is a Review instance that has this Viewer and Movie
+#review.movie AND review.viewer  NEEDS review.viewer == self
+#review holds both viewer and movie
+#checking to see if the passed movie is a reviewed movie
     def has_reviewed_movie(self, movie):
-        if self.movie == movie.review:
+        if movie in self.reviewed_movies():
             return True
+        
 
 from classes.review import Review
 from classes.movie import Movie
